5586ee96310d3c4f37f64aee002bba43ff829c94
‘QiuYeDx’ <qiuyedx@foxmail.com>
2023-07-13 15:33:56 +0800

    v0.1.0 beta

--- src/main/java/com/example/selenium_test/SeleniumTestApplication.java
+++ src/main/java/com/example/selenium_test/SeleniumTestApplication.java
@@ -1,25 +1,15 @@
 package com.example.selenium_test;
 
-import org.apache.log4j.Logger;
-import org.openqa.selenium.WebDriver;
+import com.example.selenium_test.selenium_questionnaire.SeleniumQuestionnaire;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import selenium_questionnaire.SeleniumQuestionnaire;
-
-import java.sql.*;
-import java.util.Calendar;
-import java.util.UUID;
-import java.util.Date;
 
 @SpringBootApplication
+
 public class SeleniumTestApplication {
-    static Logger log = Logger.getLogger(SeleniumTestApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(SeleniumTestApplication.class, args);
-        SeleniumQuestionnaire seleniumQuestionnaire = new SeleniumQuestionnaire();
-        WebDriver driver =  seleniumQuestionnaire.initWebDriver("/Users/baizihan/Documents/学习空间/Java/selenium_test/src/main/resources/chromedriver");
-
     }
 
 
---
+++ src/main/java/com/example/selenium_test/dao/Entity/InstructionEntity.java
@@ -0,0 +1,31 @@
+package com.example.selenium_test.dao.Entity;
+
+public class InstructionEntity {
+    private String type;
+    private String element;
+    private String param;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getElement() {
+        return element;
+    }
+
+    public void setElement(String element) {
+        this.element = element;
+    }
+
+    public String getParam() {
+        return param;
+    }
+
+    public void setParam(String param) {
+        this.param = param;
+    }
+}
